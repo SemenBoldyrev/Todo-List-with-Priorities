3133b875e19673f379db89ea6c90a19c887f813d
Deniss Mihhailov <krasava1081@gmail.com>
2026-03-14 12:47:32 +0200

    Add SaveData and LoadData functions

--- Saves/SaveDict.py
+++ Saves/SaveDict.py
@@ -1,6 +1,24 @@
-save = [{
-    'id': 1,
-    'description': 'Complete project proposal',
-    'priority': 'High',
-    'completed': False
-}]
+data_holder = [
+    {
+        "id": 1,
+        "description": "Complete project proposal",
+        "priority": "High",
+        "completed": False
+    }
+]
+
+
+def SaveData(data: list):
+    """Saves task data into temporary storage."""
+    global data_holder
+
+    if not isinstance(data, list):
+        return False
+
+    data_holder = data
+    return True
+
+
+def LoadData() -> list:
+    """Loads task data from temporary storage."""
+    return data_holder
