33e75b5d855f0c8b5a88b715f765ab8c680e7b23
Semen Boldyrev <233458958+SemenBoldyrev@users.noreply.github.com>
2026-03-14 13:15:09 +0200

    Manager bus base implimentation

---
+++ ManagerBus.py
@@ -0,0 +1,13 @@
+import Saves.SaveDict
+import Saves.PriorityTypes
+import Controllers.TaskController
+import TaskCreator.TaskCreator
+import Filter.Filter
+import Interface.InterfaceMain
+
+saves = Saves.SaveDict
+priorityTypes = Saves.PriorityTypes
+taskController = Controllers.TaskController
+taskCreator = TaskCreator.TaskCreator
+taskFilter = Filter.Filter
+interface = Interface.InterfaceMain
